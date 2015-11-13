5f9732eaf25f2c01229cd6881db5e3f33574ef1a
Antony <anton.plotnikov@gmail.com>
2015-11-13 09:03:02 +0000

    only [images] storage available

--- common/collections/collectionFS.js
+++ common/collections/collectionFS.js
@@ -11,11 +11,13 @@ ReactionCore.Collections.Media = new FS.Collection("Media", {
     new FS.Store.IFDS3("images", {
       path: Meteor.settings['TMP_UPLOAD_PATH'],
       s3auth: Meteor.settings['s3auth']
-    }),
+    })
+    /*,
     new FS.Store.FileSystem("large",     { path: Meteor.settings['TMP_UPLOAD_PATH'] }),
     new FS.Store.FileSystem("medium",    { path: Meteor.settings['TMP_UPLOAD_PATH'] }),
     new FS.Store.FileSystem("small",     { path: Meteor.settings['TMP_UPLOAD_PATH'] }),
     new FS.Store.FileSystem("thumbnail", { path: Meteor.settings['TMP_UPLOAD_PATH'] })
+    */
   ],
   filter: {
     allow: {
